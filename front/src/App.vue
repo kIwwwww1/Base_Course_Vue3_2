05e74fd5b0233574e2ffa282815a9b63e7fa7840
kIwwwww1 <danil29070@gmail.com>
2026-02-12 17:57:49 +0300

    Base course dy Vue3 2.0

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,11 +1,25 @@
 <template>
+  <div className="reg_div">
+    <input type="text" v-model="user_name" placeholder="Имя">
+    <input type="password" v-model="user_password" placeholder="Пароль">
+    <input type="email" v-model="user_email" placeholder="Email">
+    <button @click="add_user">Отправить</button>
+  </div>
 
-  <h3>{{ info }}</h3>
-  <p>{{ msg }}</p>
-  <div class="main_div">
-    <button type="button" @click="user_data()" class="button">Отправит #1</button>
-    <button type="button" @mouseenter="user_data2()" class="button">Отправить #2</button>
-    <p id="_couner">{{ counter }}</p>
+  <div v-if="users.length == 0" className='new_users'>
+    <p>Пользователи не найдены</p>
+  </div>
+  <div v-else-if="users.length == 5" className='new_users'>
+    <p>Превышен лимит пользователей</p>
+  </div>
+  <div v-else className='new_users'>
+    <p>Все условия не верны, работает v-else</p>
+  </div>
+
+  <div className='new_users' v-for="(user, user_id) in users" :key="user_id">
+    <p id="user_name">{{user.name}}</p>
+    <p id="user_password">{{user.password}}</p>
+    <p id="user_email">{{user.email}}</p>
   </div>
 
 </template>
@@ -15,65 +29,73 @@
 export default {
   data() {
     return {
-      info: 'Hello World #1',
-      msg: 'Hello World #2',
-      counter: 0,
+      users: [],
+      user_name: '',
+      user_password: '',
+      user_email: '',
     }
   },
   methods: {
-    user_data() {
-      this.info = 'Новая кнопка #1'
-      this.counter += 1
-      console.log('Hello World #1')
-      console.log(`Счетчик: ${this.counter}`)
-    },
-    user_data2() {
-      this.msg = 'Новая кнопка #2'
-      console.log('Hello World #2')
+    add_user() {
+      if (this.user_name == '' | this.user_password == '' | this.user_email == '') {
+        window.alert('Заполните все поля')
+        return
+      }
+      this.users.push({
+        name: this.user_name,
+        password: this.user_password,
+        email: this.user_email,
+      })
     }
   }
 }
 
 </script>
 
-<style scoped>
+<style>
 
 * {
-  display: flex;
-  padding: 0px;
   margin: 0px;
+  padding: 0px;
 }
 
-h3 {
-  font-weight: lighter;
-}
-
-p {
-  color: rgb(156, 156, 219);
-}
-
-.main_div {
+.reg_div {
   display: flex;
   flex-direction: column;
   align-items: center;
-  background-color: aliceblue;
+  padding: 10px 10px;
 }
 
-.button {
-  padding: 10px 20px;
-  margin: 10px;
-  border: 1px solid #42b983;
-  border-radius: 5px;
-  background-color: #42b983;
-  color: #fff;
-  font-size: 16px;
-  cursor: pointer;
-}
-
-#_couner {
+.reg_div input {
+  width: 150px;
+  height: 30px;
   padding: 10px 15px;
-  margin: 20px;
-  font-size: 5em;
+  border-radius: 7px;
+  border: 1px solid black;
+  margin: 5px 0px;
+}
+
+.reg_div button{
+  width: 100px;
+  height: 30px;
+  margin: 5px 0px;
+  background-color: green;
+  cursor: pointer;
+  border-style: none;
+  color: white;
+  border-radius: 6px;
+}
+
+.new_users {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 10px 10px;
+  background-color: rgb(255, 251, 251);
+  width: 300px;
+  margin: 10px auto;
+  border-radius: 7px;
+  border: 3px solid black ;
 }
 
 </style>
